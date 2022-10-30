8138b0833c601c181134e52ab5397e3e9a201450
liyijiadou2020 <1724881015@qq.com>
2022-10-30 12:08:07 +0300

    使用着色器程序来改变三角形的颜色。

--- test/Chap01/Sample_1_3_EBO/lyj_openglwidget.cpp
+++ test/Chap01/Sample_1_3_EBO/lyj_openglwidget.cpp
@@ -14,30 +14,6 @@ LYJ_OpenGLWidget::LYJ_OpenGLWidget(QWidget *parent) : QOpenGLWidget(parent)
 
 }
 
-LYJ_OpenGLWidget::~LYJ_OpenGLWidget()
-{
-    makeCurrent();
-    glDeleteBuffers(1,&VBO);
-    glDeleteVertexArrays(1,&VAO);
-    doneCurrent();
-}
-
-
-void LYJ_OpenGLWidget::drawShape(LYJ_OpenGLWidget::Shape shape)
-{
-    m_shape=shape;
-    update();
-}
-
-void LYJ_OpenGLWidget::setWirefame(bool wireframe)
-{
-    makeCurrent();
-    if(wireframe) glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-    else glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-    update();
-    doneCurrent();
-}
-
 void LYJ_OpenGLWidget::initializeGL()
 {
     initializeOpenGLFunctions();
@@ -47,7 +23,7 @@ void LYJ_OpenGLWidget::initializeGL()
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); // VBO需要指定类型。绑定之后这个VBO就会记录GL_ARRAY_BUFFER的状态了
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // VBO需要指定类型
 
     //【VBO管理】为当前绑定到target的缓冲区对象创建一个新的数据存储。
     //如果data不是NULL，则使用来自此指针的数据初始化数据存储
@@ -57,19 +33,8 @@ void LYJ_OpenGLWidget::initializeGL()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // VBO休息
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // 链接和编译着色器
-    bool success;
-    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex,":/shaders/shapes.vert");
-    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment,":/shaders/shapes.frag");
-    success=shaderProgram.link();
-    if(!success)
-        qDebug()<<"ERROR:"<<shaderProgram.log();
-
-    // 【VAO最后休息！！】
-    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // VBO休息
+    glBindVertexArray(0); // VAO休息
 }
 
 void LYJ_OpenGLWidget::resizeGL(int w, int h)
@@ -82,11 +47,9 @@ void LYJ_OpenGLWidget::paintGL()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // 绑定编译好的着色器程序
-    shaderProgram.bind();
-
-    // 先绑定【VAO】再进行绘制
+    // 先绑定再进行绘制
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3); // 绘制
+    // 绘制
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
 } // paintGL
