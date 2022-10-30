0d7a0365c274ffb7344c8cefb768f018f3815b50
liyijiadou2020 <1724881015@qq.com>
2022-10-30 13:19:07 +0300

    Chap03:纹理。增加了两个纹理：墙纸和笑脸

---
+++ test/Chap03/Sampl_3_1_Texture/axbopenglwidget.cpp
@@ -0,0 +1,125 @@
+﻿#include "axbopenglwidget.h"
+
+unsigned int VBO, VAO,EBO;
+float vertices[] = {
+    // positions      // colors             // texture coords
+    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,     1.0f, 1.0f, // top right
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,    1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,    0.0f, 1.0f // top left
+};
+
+
+unsigned int indices[] = { // note that we start from 0!
+                           0, 1, 3, // first triangle
+                           1, 2, 3 // second triangle
+                         };
+
+AXBOpenGLWidget::AXBOpenGLWidget(QWidget *parent) : QOpenGLWidget(parent)
+{
+
+}
+
+AXBOpenGLWidget::~AXBOpenGLWidget()
+{
+    makeCurrent();
+    glDeleteBuffers(1,&VBO);
+    glDeleteBuffers(1,&EBO);
+    glDeleteVertexArrays(1,&VAO);
+    doneCurrent();
+}
+
+void AXBOpenGLWidget::drawShape(AXBOpenGLWidget::Shape shape)
+{
+    m_shape=shape;
+    update();
+}
+
+void AXBOpenGLWidget::setWirefame(bool wireframe)
+{
+    makeCurrent();
+    if(wireframe)
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    else
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    update();
+    doneCurrent();
+}
+
+void AXBOpenGLWidget::initializeGL()
+{
+    initializeOpenGLFunctions();
+
+    //创建VBO和VAO对象，并赋予ID
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    //绑定VBO和VAO对象
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //为当前绑定到target的缓冲区对象创建一个新的数据存储。
+    //如果data不是NULL，则使用来自此指针的数据初始化数据存储
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //<位置>告知显卡如何解析缓冲里的属性值，开启第一个属性值
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);    
+    glEnableVertexAttribArray(0);
+
+    //<颜色>
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //<纹理>增添一个属性2：纹理坐标
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //【着色器程序】
+    bool success;
+    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex,":/shaders/shapes.vert");
+    shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment,":/shaders/shapes.frag");
+    success=shaderProgram.link();
+    if(!success)
+        qDebug()<<"ERR:"<<shaderProgram.log();
+
+    //【EBO】
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //【纹理】实例化QopenGLTexture对象（已经将图片加载进来）
+    textureWall=new QOpenGLTexture(QImage(":/images/images/wall.jpg").mirrored());
+    textureSmile=new QOpenGLTexture(QImage(":/images/images/awesomeface.png").mirrored());
+    // 在着色器程序被链接成功后才能使用setUniformValue
+    shaderProgram.bind();
+    shaderProgram.setUniformValue("textureWall",0);
+    shaderProgram.setUniformValue("textureSmile",1);
+    glBindVertexArray(0);
+}
+
+void AXBOpenGLWidget::resizeGL(int w, int h)
+{
+    Q_UNUSED(w);Q_UNUSED(h);
+    //glViewport(0, 0, w, h);
+}
+
+void AXBOpenGLWidget::paintGL()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    shaderProgram.bind();
+    glBindVertexArray(VAO);
+
+    switch (m_shape) {
+    case Rect:       
+        textureWall->bind(0); // glActiveTexture(GL_TEXTURE0);
+        textureSmile->bind(1); //glActiveTexture(GL_TEXTURE1);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+        break;
+    default:
+        break;
+    }
+}
---
+++ test/Chap03/Sampl_3_1_Texture/axbopenglwidget.h
@@ -0,0 +1,35 @@
+﻿#ifndef AXBOPENGLWIDGET_H
+#define AXBOPENGLWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+class AXBOpenGLWidget : public QOpenGLWidget,QOpenGLFunctions_3_3_Core
+{
+    Q_OBJECT
+public:
+    enum Shape{None,Rect,Circle,Triangle};
+    explicit AXBOpenGLWidget(QWidget *parent = nullptr);
+    ~AXBOpenGLWidget();
+
+    void drawShape(Shape shape);
+    void setWirefame(bool wireframe);
+protected:
+    virtual void initializeGL();
+    virtual void resizeGL(int w, int h);
+    virtual void paintGL();
+signals:
+
+public slots:
+private:
+    Shape m_shape;
+    QOpenGLShaderProgram shaderProgram;
+    // Chap03：纹理
+    QOpenGLTexture * textureWall;
+    QOpenGLTexture * textureSmile;
+};
+
+
+
+#endif // AXBOPENGLWIDGET_H
